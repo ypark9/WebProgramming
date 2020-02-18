e89404be0c57e74910ea1a933d9e4150e7f430d1
ypark9@ncsu.edu <ypark9@ncsu.edu>
2020-02-17 23:40:30 -0500

    Start coding interview preparation

---
+++ JQuery/index.js
@@ -0,0 +1,7 @@
+$("h1").addClass("big-title");
+//$("h1").removeClass("big-title");
+//$("h1").hasClass("big-title");
+//$("h1").text("Hi!");
+//$().html("<em>No!</em>")
+
+$("img").attr("src", "tom3.png");
