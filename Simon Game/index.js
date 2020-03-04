d9897569dac009332d3198fc0d4a3f026b3f927e
ypark9@ncsu.edu <ypark9@ncsu.edu>
2020-03-03 23:30:09 -0500

    add button effects

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,34 +1,42 @@
-
-
-function initGame() {
-    this.game = {};
-    this.totalCount = 1;
-    this.isStarted = true;
-    console.log(initGame);
-}
-
 function gameLevelGenerator() {
-    let count = this.totalCount;
+    let count = window.game.stage;
     for(let i = 0; i < count; i++){
         //console.log(i);
-        if(this.game[totalCount])
-        push(Math.floor(Math.random() * 4));     // returns a random integer from 1 to 4
-        else
-        this.game[totalCount] = [Math.floor(Math.random() * 4)];
+        if(window.game.content) {
+            window.game.content. push(Math.floor(Math.random() * 4));     // returns a random integer from 1 to 4
+        }
+        else {
+            window.game.content = [];
+            window.game.stage = [Math.floor(Math.random() * 4)];
+        }
     }
 
-    for(let step of this.game[totalCount]){
+    for(let step of window.game.stage){
         if(step === 0){
             $(".green").trigger('click');
+            $('.green').addClass('pressed');
+            setTimeout(() => {
+                $('.green').removeClass("pressed");
+            }, 200);
         }
         else if (step === 1){
             $(".red").trigger('click');
-        }
+            $('.red').addClass('pressed');
+            setTimeout(() => {
+                $('.red').removeClass("pressed");
+            }, 200);        }
         else if(step === 2){
             $(".yellow").trigger('click');
-        }
+            $('.yellow').addClass('pressed');
+            setTimeout(() => {
+                $('.yellow').removeClass("pressed");
+            }, 200);        }
         else if (step === 3){
             $(".blue").trigger('click');
+            $('.blue').addClass('pressed');
+            setTimeout(() => {
+                $('.blue').removeClass("pressed");
+            }, 200);
         }
     }
 }
@@ -41,12 +49,16 @@ $(".btn").click(function(){
 
 document.addEventListener('keydown', function(e) {    
     if(e.code === "KeyA")
-    {
-        console.log(this.isStarted);
-        if(this.isStarted)
-            alert("Game is started, you babo Pumba!")
-        else
-            initGame();
+    {   
+        if($('h1').text() === 'Press A Key to Start') {
+            $('h1').text('Stage 1');
+            window.game = {};
+            window.game['stage'] = 1;
+            gameLevelGenerator();
+        }
+        else {
+            alert('Game is already started! :) ');
+        }
     }
 });
 
