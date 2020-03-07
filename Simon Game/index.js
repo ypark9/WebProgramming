47d2b533a0e740fe76e030e1204afa82c75dfc83
ypark9@ncsu.edu <ypark9@ncsu.edu>
2020-03-06 21:24:01 -0500

    update simon game

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,50 +1,93 @@
 function gameLevelGenerator() {
+    window.game.inputArr = [];
     let count = window.game.stage;
     for(let i = 0; i < count; i++){
         //console.log(i);
         if(window.game.content) {
-            window.game.content. push(Math.floor(Math.random() * 4));     // returns a random integer from 1 to 4
+            window.game.content.push(Math.floor(Math.random() * 4));     // returns a random integer from 0 to 3
         }
         else {
-            window.game.content = [];
-            window.game.stage = [Math.floor(Math.random() * 4)];
+            //window.game.content = [];
+            window.game.content = [Math.floor(Math.random() * 4)];
         }
     }
 
-    for(let step of window.game.stage){
+    for(let step of window.game.content){
+        (function(step){
+            setTimeout(function(){
+                console.log('value is ', step);
+            }, 3000);
+        })(step);
+
         if(step === 0){
-            $(".green").trigger('click');
+            // $(".green").trigger('click');
+            playSound('green');
             $('.green').addClass('pressed');
             setTimeout(() => {
                 $('.green').removeClass("pressed");
             }, 200);
         }
         else if (step === 1){
-            $(".red").trigger('click');
+            playSound('red');
             $('.red').addClass('pressed');
             setTimeout(() => {
                 $('.red').removeClass("pressed");
             }, 200);        }
         else if(step === 2){
-            $(".yellow").trigger('click');
+            playSound('yellow');
             $('.yellow').addClass('pressed');
             setTimeout(() => {
                 $('.yellow').removeClass("pressed");
             }, 200);        }
         else if (step === 3){
-            $(".blue").trigger('click');
+            playSound('blue');
             $('.blue').addClass('pressed');
             setTimeout(() => {
                 $('.blue').removeClass("pressed");
             }, 200);
         }
+        setTimeout(2000);
     }
 }
 
-$(".btn").click(function(){    
-    let soundAddress = "sounds/" + this.id + ".mp3";
-    let sound = new Audio(soundAddress);
-    sound.play();
+$(".btn").click(function(){ 
+    playSound(this.id);
+    console.log(this.id);
+    
+    let choice = 1;
+    if(this.id  === "green"){
+        choice = 0;
+    }
+    else if (this.id  === 'red'){
+        choice = 1;
+    }
+    else if(this.id  === 'yellow'){
+        choice = 2;
+    }
+    else if (this.id  === "blue"){
+        choice = 3;
+    }
+    console.log('choice = ' + choice);
+    
+    window.game.inputArr.push(choice);
+    console.log(window.game.inputArr);
+    console.log(window.game.content);
+
+    
+    if(window.game.content.length === window.game.inputArr.length ){
+        if(JSON.stringify(window.game.content ) === JSON.stringify(window.game.inputArr)){
+            alert('Correct!');
+            window.game.stage ++;
+            let stageName = 'Stage ' + window.game.stage; 
+            $('h1').text(stageName);
+            gameLevelGenerator();
+        }else {
+            alert('Uh oh..!');
+            let stageName = 'Game over '; 
+            $('h1').text(stageName);
+            window.game.stage = 0;
+        }
+    }
 });
 
 document.addEventListener('keydown', function(e) {    
@@ -62,4 +105,10 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
+
+function playSound(name) {
+    let soundAddress = "sounds/" + name + ".mp3";
+    let sound = new Audio(soundAddress);
+    sound.play();
+}
 
