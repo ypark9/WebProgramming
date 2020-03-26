514b3c6612bff899e769aabface63dbc585e1406
ypark9@ncsu.edu <ypark9@ncsu.edu>
2020-03-25 22:17:58 -0400

    well..

--- localServer/server.js
+++ localServer/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.listen('3000', function() {
-    //console.log("server is runnign at :3000");
+    console.log("server is runnign at :3000");
     
 });
